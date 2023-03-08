821a44e3c6cdb9c5b7be6f9b32d6811c59fb67b1
hasunghwa <tjdghk3920@naver.com>
2023-03-08 16:23:34 +0900

    feat : 게시글을 서버에서 가져와 출력하도록 추가

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { Link } from 'react-router-dom';
+import { Button } from 'antd';
+import TableComponent from '@/components/TableComponent';
+import MainLayout from '@/Layout/MainLayout';
+import PostService from '@/api/PostService';
 
 const Home = () => {
-  return <div>홈화면</div>;
+  const [posts, setPosts] = useState();
+
+  const getPosts = async () => {
+    const response = await PostService.getPosts();
+    setPosts(response);
+  };
+
+  useEffect(() => {
+    if (!posts) {
+      getPosts();
+    }
+  }, [posts]);
+
+  return (
+    <MainLayout title="Home">
+      <TableComponent posts={posts} />
+      <Link to="/post/add">
+        <Button type="primary" htmlType="submit">
+          작성하기
+        </Button>
+      </Link>
+    </MainLayout>
+  );
 };
 
 export default Home;
